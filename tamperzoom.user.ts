958313b4a6f50a5673ed8e00cb1a1370830262ae
Marcin Mielniczuk <marmistrz.dev@zoho.eu>
2020-10-23 13:55:03 +0200

    Show toolbar when toggling audio/video

--- tamperzoom.user.ts
+++ tamperzoom.user.ts
@@ -8,9 +8,18 @@
 // @grant        none
 // ==/UserScript==
 
+const getFirstElementByClassName = (classname: string): HTMLElement | undefined => {
+    return document.getElementsByClassName(classname)[0] as HTMLElement;
+}
+
 const clickButton = (classname: string) => {
-    const button = document.getElementsByClassName(classname)[0] as HTMLElement;
-    button.click();
+    const button = getFirstElementByClassName(classname);
+    button?.click();
+}
+
+const showToolbar = () => {
+    const footer = getFirstElementByClassName('footer');
+    footer!!.className = 'footer';
 }
 
 const toggleAudio = () => clickButton('join-audio-container__btn');
@@ -20,8 +29,10 @@ document.addEventListener('keydown', e => {
     if (!e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) {
         if (e.key == 'a') {
             toggleAudio();
+            showToolbar();
         } else if (e.key == 'v') {
             toggleVideo();
+            showToolbar();
         }
     }
 }, false);
